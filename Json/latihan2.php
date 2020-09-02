269f229dab648bdf2f385364019b006b777414c9
arifin23 <arifingdr@gmail.com>
2020-09-02 20:15:45 +0700

    Removed folder from repository

--- Json/latihan2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$data = file_get_contents('Coba.json');
-$mahasiswa = json_decode($data, true);
-
-// var_dump($mahasiswa);
-echo $mahasiswa[1]["Pembimbing"]["pembimbing1"];
-echo ",";
-echo $mahasiswa[1]["hobby"][0];
