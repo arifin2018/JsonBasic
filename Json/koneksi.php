782bd977abce41e601a9551412df604edeb1c79a
arifin23 <arifingdr@gmail.com>
2020-09-05 15:01:48 +0700

    fixed unique

---
+++ Json/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+// $dbhost = 'localhost';
+// $dbuser = 'root';
+// $dbpass = '';
+// $dbname = 'crud_ci4';
+
+// $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+$dbh = new PDO ('mysql:host=localhost;dbname=crud_ci4','root','');
+
+?>
--- wpu_hut/js/script.js
+++ wpu_hut/js/script.js
@@ -1,8 +1,9 @@
-$.getJSON("data/pizza.json", function (data) {
-  let menu = data.menu;
-  $.each(menu, function (i, data) {
-    $("#daftar-menu").append(
-      '<div class="col-md-4 col-sm-6 col-xs-12"><div class="card mb-3"><img src="img/pizza/' +
+function tampilkanSemuaMenu() {
+  $.getJSON("data/pizza.json", function (data) {
+    let menu = data.menu;
+    $.each(menu, function (i, data) {
+      $("#daftar-menu").append(
+        '<div div class= "col-md-4 col-sm-6 col-xs-12" > <div class="card mb-3 "><img src="img/pizza/' +
         data.gambar +
         '" class="gambar"><div class="card-body"><h5 class="card-title">' +
         data.nama +
@@ -11,9 +12,11 @@ $.getJSON("data/pizza.json", function (data) {
         '</p><h2 class="cards card-title "><strong> Rp.' +
         data.harga +
         '</strong></h2><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
-    );
+      );
+    });
   });
-});
+}
+tampilkanSemuaMenu();
 
 $(".dropdown-item").on("click", function () {
   $(".dropdown-item").removeClass("active");
@@ -21,21 +24,39 @@ $(".dropdown-item").on("click", function () {
 
   let dropdown = $(this).html();
   // console.log(dropdown);
-  $(".dropdown-toggle").html(dropdown);
+  $("#tuyul").html(dropdown);
+
+  if (dropdown == 'All Menu') {
+
+    $.getJSON("data/pizza.json", function (data) {
+      var names = data.menu;
+      var uniqueNames = [];
+      $.each(names, function (i, el) {
+        if ($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+      });
+      tampilkanSemuaMenu();
+      return;
+    });
+  }
+
 
   $.getJSON("data/pizza.json", function (data) {
     let menu = data.menu;
-    let content = "";
+    let content = '';
 
     $.each(menu, function (i, data) {
-      $("#ga").append(
-        '<a class="dropdown-item" id="ga" href="#"></a>+data.kategori'
-      );
+      if (data.kategori == dropdown.toLowerCase()) {
+        content += '<div div class= "col-md-4 col-sm-6 col-xs-12" > <div class="card mb-3 "><img src="img/pizza/' +
+          data.gambar +
+          '" class="gambar"><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><p class="card-text">' +
+          data.deskripsi +
+          '</p><h2 class="cards card-title "><strong> Rp.' +
+          data.harga +
+          '</strong></h2><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>';
+      }
     });
-
-    $.each(menu, function (i, data) {
-      if (typeof data.kategori == kategori);
-      data.kategori == data.kategori.toLowerCase();
-    });
+    $('#daftar-menu').html(content);
   });
 });
